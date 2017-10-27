8eff7086f5c5efab2fcb28dcb92c368bf7015674
kukjevov <kukjevov@gmail.com>
2017-10-27 13:29:03 +0200

    BUGFIX - fixed forgotten exports fix

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 
 export {ResponseType, LocationHeaderAndJsonResponse, LocationHeaderResponse} from './rest/responseType';
-export {Body,PlainBody,BaseUrl,Cache,DefaultHeaders,DELETE,GET,HEAD,Header,Headers,JsonContentType,FormDataContentType,ParameterTransform,Path,POST,Produces,PUT,Query,QueryObject,ResponseTransform,RESTClient} from './rest/common';
+export {Cache} from './rest/cache';
+export {Body,BaseUrl,DefaultHeaders,DELETE,GET,HEAD,Header,Headers,JsonContentType,ParameterTransform,Path,POST,Produces,PUT,Query,QueryObject,ResponseTransform,RESTClient,FullHttpResponse,ReportProgress} from './rest/common';
 export {BrowserTransferStateRestModule} from './modules/browserTransferStateRest.module';
 export {TransferStateService, TRANSFER_STATE_NAME} from './transferState/transferState.service';
