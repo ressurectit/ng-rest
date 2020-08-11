d32e27dda5e6ffe004ae93f789c5476b83ea09e1
kukjevov <kukjevov@gmail.com>
2020-08-11 09:09:33 +0200

    INT: fixed forgotten missing export

--- src/rest.ts
+++ src/rest.ts
@@ -1,5 +1,5 @@
 export {ResponseType, LocationHeaderAndJsonResponse, LocationHeaderResponse, BlobAndFilenameResponse} from './rest/responseType';
 export {Cache} from './rest/cache';
-export {Body,BaseUrl,DefaultHeaders,DELETE,GET,HEAD,Header,Headers,JsonContentType,ParameterTransform,Path,POST,Produces,PUT,Query,QueryObject,ResponseTransform,RESTClient,FullHttpResponse,ReportProgress, DisableInterceptor} from './rest/common';
+export {Body,BaseUrl,DefaultHeaders,DELETE,GET,HEAD,Header,Headers,JsonContentType,ParameterTransform,Path,POST,Produces,PUT,Query,QueryObject,ResponseTransform,RESTClient,FullHttpResponse,ReportProgress, DisableInterceptor, ProgressIndicatorGroup} from './rest/common';
 export {RestTransferStateService} from './transferState/restTransferState.service';
 export {AdditionalInfoPropertyDescriptor} from './rest/additionalInfoPropertyDescriptor';
